3ba7a33f9588fea02ecac47c534de595f7c576c5
Kaj Grant <kajgrant@gmail.com>
2023-04-12 14:38:24 -0700

    more cleanup

--- Vivado/sources/hdl_coder_lms/setup.m
+++
@@ -1,8 +0,0 @@
-% Input audio file
-audio = 'original_speech.wav';
-[sampled_audio, fs] = audioread(audio, [1 200000]);
-
-samples = length(sampled_audio);
-
-audio_in.signals.values = sampled_audio(:,1);
-audio_in.time =[];
